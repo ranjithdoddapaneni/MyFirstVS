abf51cbdfed2142431f4c06b7847242eb96102e2
RanjithDoddapaneni <Ranjith.Doddapaneni@gmail.com>
2018-06-07 10:19:00 -0400

    Login

---
+++ LoginPage/LoginPage/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(LoginPage.Startup))]
+namespace LoginPage
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
