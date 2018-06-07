9be6d48efd35137a4548b198ddcb02f83cf9b9eb
RanjithDoddapaneni <Ranjith.Doddapaneni@gmail.com>
2018-06-07 09:44:40 -0400

    Changes

--- HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/Program.cs
@@ -10,7 +10,8 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            System.Console.Writeline("Hello World");
+            System.Console.WriteLine("Hello World");
+            System.Console.ReadLine();   
         }
     }
 }
